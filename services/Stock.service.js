fb728031a4138846d4fee921f34289b0d5f9bffb
ashik-ahmed <ashikahmed121@gmail.com>
2024-05-17 15:57:00 +0600

    delete stock with stockHistory api implemented

--- services/Stock.service.js
+++ services/Stock.service.js
@@ -1,4 +1,5 @@
 const Stock = require("../models/Stock");
+const StockHistory = require("../models/StockHistory");
 
 exports.createNewStockService = async (data) => {
     // console.log(data);
@@ -12,6 +13,16 @@ exports.getAllStockService = async () => {
 }
 
 exports.deleteStockByIdService = async (id) => {
+    const stock = await Stock.findById(id);
+    // console.log(stock);
+    if (stock?.stockHistory?.length > 0) {
+        const deleteStockHistory = await StockHistory.deleteMany({ _id: { $in: stock.stockHistory } });
+        console.log(deleteStockHistory);
+        if (deleteStockHistory?.deletedCount > 0) {
+            const result = await Stock.findByIdAndDelete(id);
+            return result;
+        }
+    }
     const result = await Stock.findByIdAndDelete(id);
     return result;
 }
@@ -20,7 +31,6 @@ exports.getStockByIdService = async (id) => {
     // console.log(id);
     const result = await Stock.findById(id).populate("aircraftId", "aircraftName").populate("stockHistory");
     return result;
-
 }
 
 exports.getStockHistoryByStockIdService = async (id) => {
